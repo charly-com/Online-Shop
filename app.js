3737d7cc684f6c91363e94cbe6acc1908e0a7cdd
Decagon <decagon@Decagons-MacBook-Air.local>
2022-11-13 23:53:01 +0100

    added the MVC

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
+const errorController = require('./controllers/error')
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
-const userRoutes = require('./routes/user');
+const userRoutes = require('./routes/shop');
 
 
 app.use(bodyParser.urlencoded({ extended: false })); 
@@ -14,8 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(userRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-})
+app.use(errorController.get404)
 
 app.listen(3030);
--- routes/admin.js
+++ routes/admin.js
@@ -2,17 +2,12 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 const rootDir = require('../util/path')
+const productsController = require('../controllers/products');
 
 
+router.get('/add-product', productsController.getAddProduct);
 
-router.get('/add-product', (req, res) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-})
-
-router.post('/add-product', (req, res) => {
-    console.log(req.body)
-    res.redirect('/')
-})
+router.post('/add-product', productsController.postAddProduct);
 
 
 module.exports = router;
